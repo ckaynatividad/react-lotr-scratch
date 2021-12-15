8ac3082d08cf02e74c329418008a2e32a373f80e
ckaynatividad <natividad.ckay@gmail.com>
2021-12-14 17:00:10 -0800

    testing if in branch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { BrowserRouter, NavLink, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <BrowserRouter>
+      <header>
+        <NavLink to="/" data-testid="/">
+        Home
+        </NavLink>
+        <NavLink to="/films" data-testid="film-link">
+        films
+        </NavLink>
+        <NavLink to="/characters" data-testid="char-link">
+        characters
+        </NavLink>
+        <NavLink to="/books" data-testid="book-link">
+        books
+        </NavLink>
+      </header>
+      <Switch>
+    <Route path="/" exact>
+      <h1>Home</h1>
+      <Home />
+    </Route>
+    <Route path="/films" exact>
+      <h1>films</h1>
+      <FilmL
+    </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
---
+++ src/views/Main/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Main() {
+  return (
+    <div className="home">
+      <h1>huwwo</h1>
+    </div>
+  );
+}
