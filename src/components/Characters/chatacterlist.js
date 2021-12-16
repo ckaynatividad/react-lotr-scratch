bfac46cdf96802c6ab1d602a9068b15477f8ba76
ckaynatividad <natividad.ckay@gmail.com>
2021-12-15 16:32:03 -0800

    removed console log

--- src/components/Characters/chatacterlist.js
+++ src/components/Characters/chatacterlist.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function CharacterList({ characters }) {
-  console.log(characters);
+//   console.log(characters);
   return (
     <div>
       {characters.map((char) => (
