295ba8699c2e143a14c3715298931a2dc193920e
ckaynatividad <natividad.ckay@gmail.com>
2021-12-15 16:18:57 -0800

    added keys

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('checks for main component text', () => {
   render(<App />);
-  const linkElement = screen.getByText('huwwo');
+  const linkElement = screen.getByText('Main Page');
   expect(linkElement).toBeInTheDocument();
 });
